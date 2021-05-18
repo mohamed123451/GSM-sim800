7d6a1d83dd632b500db81e390c6257573934c4f8
unknown <projectbess0@gmail.com>
2021-05-18 17:42:50 +0200

    implement sms func

--- GSM_interface.h
+++ GSM_interface.h
@@ -5,11 +5,25 @@
 #ifndef GSM_INTERFACE_H
 #define GSM_INTERFACE_H
 
+
+struct GSM_Responses
+{
+    u8* 
+};
+
+typedef enum{
+    echo_mode,
+     
+
+}GSM_AT;
+
+
 /********************************************
  Function name: GSM_u8Init
  Function: sending the basic command <AT> and Stop ECHO
  Formal parameter: None
- Return value: 0 -> initialization faild   1-> initialization done
+ Return value:  0 -> initialization faild   
+                1-> initialization done
  Remarks: None
 *********************************************/
 u8 GSM_u8Init(void);
@@ -24,6 +38,16 @@ u8 GSM_u8Init(void);
 *********************************************/
 u8 GSM_u8ValidateCmd(void);
 
-void GSM_sendSMS();
+/**
+ * @brief sending command to GSM module
+ *
+ *  Given a command as pointer to characters and its length, sending the command via UART to the module
+ *
+ * @param command   pointer to the string command
+ * @param len        Length of the command
+ *
+ * @return answer from the module
+ */
+void GSM_sendCommand(const u8 *command , u16 len);
 
 #endif
--- GSM_program.c
+++ GSM_program.c
@@ -70,3 +70,49 @@ u8 GSM_u8ValidateCmd(void)
 	
 	return Result;
 }
+
+
+
+void GSM_sendCommand(const u8 *command , u16 len){
+	u8 cmd_arr[len];
+	for(u16 i=0 ; i<len ; i++){
+		cmd_arr[i] = *(command+i);
+	}
+	cmd_arr[len+0] = '\r'
+	cmd_arr[len+1] = '\n'
+	MUSART1_voidTransmit(cmd_arr);
+
+}
+
+
+
+void GSM_sendSMS(u8 *msg, u8 *num){
+	//1-Set SMS system into text mode, as opposed to PDU mode
+		// cmd : AT+CMGF=1
+		GSM_sendCommand("AT+CMGF=1",9);
+		// response : OK
+
+
+
+		//counts the msg and put it in a form that can be used in my func.
+		u8 my_msg[100];
+		u16 msg_len =0
+		for(u16 i=0 ; msg[i]!='\0' ; i++){
+		my_msg[i] = *(msg+i);
+		msg_len++;
+	}
+
+	
+	u8 num_cmd[23]= "AT+CMGS=\""
+		for(u16 i=0 ; i<=13 ; i++){
+		num_cmd[i+9] = *(num+i);
+		
+	}
+	num_cmd[23] = '\"';
+
+	//2- style of the text mode command
+		// cmd : AT+CMGS="+85291234567"<CR>It is easy to send text messages.<Ctrl+z>
+		GSM_sendCommand(num_cmd,23);
+		GSM_sendCommand(my_msg, msg_len);
+
+}
